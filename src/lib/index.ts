79260a5481327cab0bce5e11836ec7157c7c7889
Charlie Hall <charlie@peachpay.app>
2024-02-29 12:26:29 -0600

    Update packages and import order

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
-import { env } from '$env/dynamic/private';
 import { env as publicEnv } from '$env/dynamic/public';
+import { env } from '$env/dynamic/private';
 
 console.log(env);
 console.log(publicEnv);
